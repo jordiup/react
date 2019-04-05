19da3b4a551f1649590478c5d04e964aa29ea70b
jordiup <jordi.hermoso.up@gmail.com>
2019-04-05 09:49:55 +0800

    ES6 Refresh - spread, destructuring

--- es6-refresh/app.js
+++ es6-refresh/app.js
@@ -3,6 +3,7 @@ var name = 'John'; // global scoping
 const age = 12; // cannot re-assign value, but can maipulate the child content which are already assigned
 let bdayMonth = 'March'; // re-assignment allowed
 
+
 // ARROW FUNCTIONS
 function sayHello(){
   console.log('Hello');
@@ -23,6 +24,7 @@ const sayYo = (name, isExcited) => {
 sayYo('Johny', true);
 
 
+
 // FOR EACH
 const fruits = ['Apples', 'Oranges', 'Grapes', 'Bananas'];
 
@@ -31,11 +33,13 @@ fruits.forEach((fruit,index) => {
 });
 
 
+
 // MAP
 const singleFruit = fruits.map((fruit) => fruit.slice(0,-1));
 singleFruit.forEach((fruit,index) => console.log(fruit)); // strips the s of the end of each fruit keyword
 
 // Maps are used in React to loop through lists.
+
 
 
 // FILTER
@@ -48,10 +52,13 @@ const people = [
 const pplFiltered = people.filter(person => person.id !== 2);
 console.log(pplFiltered);
 
+
 // SPREAD
 const arr = [1,2,3];
 const arr2 = [...arr, 4]; // spread operator
+const arr3 = [...arr.filter(num => num !== 2)];
 console.log(arr2); // Adds 4: 1,2,3,4
+console.log(arr3);
 
 const pet = {
   type: 'Dog',
@@ -65,11 +72,37 @@ const newPet = {
 
 console.log(newPet);
 
+
 // DESTRUCTURING
+const profile = {
+  user: 'John Doe',
+  address: {
+    street:'40 Main St',
+    city: 'Boston'
+  },
+  hobbies: ['movies', 'music']
+};
+
+const {user, address, hobbies} = profile;
+const {street} = profile.address;
+
+console.log(user, address.city, hobbies[0]);
+
+// Important in react because you have a lot of destructuring, you have state and you need to access/pull out the properties from the state
 
 
 // CLASSES
+class Person {
+  constructor() { // properties need a constructor (a special type of method)
+    console.log('New Person!');
+    this.name = 'Candice';
+  }
+}
 
+const person1 = new Person();
+const person2 = new Person();
+
+// A react component class is just an es6 class
 
 // SUBCLASSES
 
