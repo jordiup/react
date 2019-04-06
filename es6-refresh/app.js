823b51ca5585a24cca765a683eb521857ee62430
jordiup <jordi.hermoso.up@gmail.com>
2019-04-06 22:22:51 +0800

    ES6 Refresh - complete ✅

--- es6-refresh/app.js
+++ es6-refresh/app.js
@@ -88,23 +88,69 @@ const {street} = profile.address;
 
 console.log(user, address.city, hobbies[0]);
 
-// Important in react because you have a lot of destructuring, you have state and you need to access/pull out the properties from the state
+// Important in react because you have a lot of destructuring, you hav e state and you need to access/pull out the properties from the state
 
 
 // CLASSES
 class Person {
-  constructor() { // properties need a constructor (a special type of method)
+  // properties of a class
+  constructor(name, age) { // properties need a constructor (a special type of method)
     console.log('New Person!');
-    this.name = 'Candice';
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods of a class
+  greet(){
+    return `Hello, my name is ${this.name}, and I am ${this.age}`;
   }
 }
 
-const person1 = new Person();
-const person2 = new Person();
+const person1 = new Person('John', 33);
+const person2 = new Person('Jim', 23);
+
+console.log(person1.age);
+console.log(person1.greet());
 
 // A react component class is just an es6 class
 
-// SUBCLASSES
+/*
+  SUBCLASSES
+  This is relevant to react because in React. You want subclasses where you want to extend another class. Specifically so, you need to extend the core React component class. It includes a lot of methods we call life cycle methods.
+*/
+class Customer extends Person {
+  constructor(name,age,balance){
+    // to inherit its properties you must use 'super' from within the constructor. It takes it from the extension - Person
+    super(name,age);
+    this.balance = balance;
+  }
+
+  owes(){
+    return `${this.name} owes you $${this.balance}`
+  }
+}
+
+const customer1 = new Customer('Kevvy', 24, 200);
+console.log(customer1.name, customer1.balance);
+console.log(customer1.owes());
+
+/*
+ MODULES
+ Something you deal with a lot in react and any framework.Why? because you want to build a component, your not putting everything in one big js file. You want to important them for one app and export for another etc.
+
+ React uses babel and webpack (a module bundler), even with the React CLI, command line tool.
+
+ The following will be for demonstration only
+
+ //file 1 (file1.js)
+ export const name = 'Jeff';
+ export const nums = [1,2,3];
+ export default Person;
+
+ //file 2 (file2.js)
+ import { name } from './file1';
+ import Person from './file1'; // note no need for curly braces since exported from 'DEFAULT'
 
 
-// MODULES
+
+*/
