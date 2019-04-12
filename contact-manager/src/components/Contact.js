e09473a07182d63128c4c01b27f3b60a66ff942e
jordiup <jordi.hermoso.up@gmail.com>
2019-04-12 14:55:08 +0800

    Events in React

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+
+  state = {};
+
+  // Function that is called from the h4 button note that an arrow function must be used to maintain the state
+  onShowClick = (e) => {
+    console.log(e.target);
+  }
+
   render() {
     const { name,email,phone } = this.props.contact;
 
     return (
     <div className="card card-body mb-3">
-      <h4>{name}</h4>
+      <h4>{name} <i onClick={this.onShowClick(this)} className="fas fa-sort-down"></i></h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
@@ -17,10 +25,15 @@ class Contact extends Component {
   }
 }
 
+// Deprecated in favor of object-based declaration
+// Contact.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   email: PropTypes.string.isRequired,
+//   phone: PropTypes.string.isRequired,
+// };
+
 Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
-};
+  contact: PropTypes.object.isRequired
+}
 
 export default Contact;
