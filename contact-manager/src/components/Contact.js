68e32c95064c9a0c1b79bfa27d8c2e05404ee624
jordiup <jordi.hermoso.up@gmail.com>
2019-04-21 23:02:37 +0800

    Adding a context reducer for actions

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context'
 
 class Contact extends Component {
 
@@ -7,8 +8,9 @@ class Contact extends Component {
     showContactInfo: false
   };
 
-  onDeleteClick = () => {
-    this.props.deleteClickHandler();
+  onDeleteClick = (id,dispatch) => {
+    dispatch({type: 'DELETE_CONTACT',payload: id});
+    // this.props.deleteClickHandler();
   };
 
   // Function that is called from the h4 button note that an arrow function must be used to maintain the state (now used as an arrow function)
@@ -18,19 +20,26 @@ class Contact extends Component {
   // }
 
   render() {
-    const { name,email,phone } = this.props.contact;
+    const { id,name,email,phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
-    <div className="card card-body mb-3">
-      <h4>{name} <i onClick={ () => this.setState({ showContactInfo: !this.state.showContactInfo })} className="fas fa-sort-down" style={{cursor: 'pointer'}}></i>
-      <i className="fas fa-times" style={{cursor: 'pointer', float: 'right', color: '#E55050'}} onClick={this.onDeleteClick}></i>
-      </h4>
-      {showContactInfo ? (<ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>) : null }
-      </div>
+      <Consumer>
+        {value => {
+          const {dispatch} = value;
+          return (
+            <div className="card card-body mb-3">
+            <h4>{name} <i onClick={ () => this.setState({ showContactInfo: !this.state.showContactInfo })} className="fas fa-sort-down" style={{cursor: 'pointer'}}></i>
+            <i className="fas fa-times" style={{cursor: 'pointer', float: 'right', color: '#E55050'}} onClick={this.onDeleteClick.bind(this,id,dispatch)}></i>
+            </h4>
+            {showContactInfo ? (<ul className="list-group">
+                <li className="list-group-item">Email: {email}</li>
+                <li className="list-group-item">Phone: {phone}</li>
+              </ul>) : null }
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
