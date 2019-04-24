32215d3a3d0e7c67972941c4d3504d313af77349
jordiup <jordi.hermoso.up@gmail.com>
2019-04-24 10:16:13 +0800

    React Router setup (seperate paging)

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
-import Header from './components/layout/Header';
 import AddContact from './components/contacts/AddContact';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
 
 import { Provider } from './context';
 
@@ -27,13 +29,18 @@ class App extends Component { // (class based component), every component made w
 
     return (
       <Provider>
-      <div className="App">
-        <Header branding="Contact Manager"/>
-        <div className="container">
-          <AddContact />
-          <Contacts />
-        </div>
-      </div>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager"/>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts}/>
+                <Route exact path="/about" component={About}/>
+                <Route exact path="/contact/add" component={AddContact}/>
+              </Switch>
+            </div>
+          </div>
+        </Router>
       </Provider>
     );
   }
--- contact-manager/src/components/contacts/Contacts.js
+++ contact-manager/src/components/contacts/Contacts.js
@@ -63,7 +63,9 @@ class Contacts extends Component {
       <Consumer>
         {value => {
           const { contacts } = value;
-          return (<React.Fragment>
+          return (
+          <React.Fragment>
+            <h1 className="display-4 mb-2"><span className="text-danger">Contact</span> List</h1>
               {contacts.map(contact => (
                 <Contact 
                   key={contact.id}
