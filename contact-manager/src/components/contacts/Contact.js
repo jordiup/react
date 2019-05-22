85db7348d3c749b128f8fe44d233007bbe93d0f6
jordiup <jordi.hermoso.up@gmail.com>
2019-05-22 17:30:23 +0800

    Fixing a delete bug, test deploy to github pages

--- contact-manager/src/components/contacts/Contact.js
+++ contact-manager/src/components/contacts/Contact.js
@@ -13,6 +13,7 @@ class Contact extends Component {
   onDeleteClick = async (id,dispatch) => {
     try {
       await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+      dispatch({ type: 'DELETE_CONTACT',payload: id});
     } catch(e){
       dispatch({ type: 'DELETE_CONTACT',payload: id});
     }
