b6956055aac078961563c4575e488cc2123855ce
jordiup <jordi.hermoso.up@gmail.com>
2019-06-09 21:53:34 +0800

    Firebase project init, base ui, working

---
+++ react-fb-client-panel/src/components/layout/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <Link to="/client/add" className="btn btn-success bntn-block">
+            <i className="fa fa-plus">New</i>
+        </Link>
+    )
+}
--- react-fb-client-panel/src/index.js
+++ react-fb-client-panel/src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// Taking the div with an id of root and inserting our app in there.
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
