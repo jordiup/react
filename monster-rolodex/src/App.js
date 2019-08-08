7be07af30dad6b4e39bb463ebe48897fcc23849c
jordiup <jordi.hermoso.up@gmail.com>
2019-08-08 08:11:43 +0800

    Monster rolodex - search box as component handle change arrow function

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 import { InfoPrintout } from './components/info-printout/info-printout.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => 
-      response.json())
+    .then(response => response.json())
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState( { searchField: e.target.value } )
+  }
+
   render() {
+
+    const { monsters, searchField } = this.state; 
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
+
     return (
       <div className="App">
         
@@ -27,9 +39,13 @@ class App extends Component {
 
         
         <header className="App-header">
+
+        <SearchBox 
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        /> 
         
-        <CardList monsters={this.state.monsters}> 
-        </CardList>
+        <CardList monsters={filteredMonsters}> </CardList>
 
         </header>
     </div>
