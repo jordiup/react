9e92c459fe5aa6341cff481562e874f8b6d87aaf
jordiup <jordi.hermoso.up@gmail.com>
2019-08-16 06:50:12 +0800

    Crown 👑 - Styling signin page

--- crown-clothing/src/components/header/header.component.jsx
+++ crown-clothing/src/components/header/header.component.jsx
@@ -5,7 +5,9 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.styles.scss';
 
-const Header = () => (
+import { auth } from '../../firebase/firebase.utils';
+
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -13,7 +15,13 @@ const Header = () => (
     <div className="options">
         <Link className="option" to="/shop">Shop</Link>
         <Link className="option" to="/contact">Contact</Link>
-        <Link className="option" to="/signin">Sign in</Link>
+        {/* <Link className="option" to="/signin">Sign in</Link> */}
+        {
+          currentUser ?
+          <div className='option' onClick={() => auth.signOut()}>Sign out</div>
+          :
+          <div className='option' to='/signin'>Sign in</div>
+        }
     </div>
   </div>
 ) 
