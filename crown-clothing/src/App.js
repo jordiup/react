77aab05e2b3a77919fccd72359ba10750f36cae8
jordiup <jordi.hermoso.up@gmail.com>
2019-08-16 11:03:37 +0800

    Crown 👑 - implemented firebase utils, abilitiy to store authenticated users, finished signin handle submit method, prior to redux changes

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -32,16 +32,12 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
               }
-            }, 
-            () => {
-              console.log(this.state);
-            }
-          );
+          });
         });
       }
+      this.setState({ currentUser: userAuth });
     })
 
-    this.setState({ currentUser: userAuth });
   }
 
   componentWillUnmount() {
