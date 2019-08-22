975e9a703399f9ee08d764436ce98e220073caee
jordiup <jordi.hermoso.up@gmail.com>
2019-08-23 07:31:10 +0800

    👑 Clothing - Added addItem and removeItem toggle functionality to checkout item component

--- crown-clothing/src/redux/cart/cart.utils.js
+++ crown-clothing/src/redux/cart/cart.utils.js
@@ -9,9 +9,26 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
       ? {... cartItem, quantity: cartItem.quantity + 1 }
       : cartItem  
     )
-
   }
 
   // quantity property of 1 is used when the item doesn't exist yet (first time added to cart)
   return [...cartItems, {...cartItemToAdd, quantity: 1}];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  if (existingCartItem.quantity === 1){
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  return cartItems.map(
+    cartItem => 
+    cartItem.id === cartItemToRemove.id ?
+    { ...cartItem, quantity: cartItem.quantity -1 }
+    : cartItem
+  );
+
+}
