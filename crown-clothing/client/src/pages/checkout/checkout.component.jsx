05369b2f12f0a31a9d5ecc0477ed47a38e35e4cd
jordiup <jordi.hermoso.up@gmail.com>
2019-09-04 06:29:20 +0800

    👑 Clothing - Directory arrangement for firebase deploy, CVV added to test card details, git ignore updates

--- crown-clothing/client/src/pages/checkout/checkout.component.jsx
+++ crown-clothing/client/src/pages/checkout/checkout.component.jsx
@@ -47,7 +47,7 @@ const CheckoutPage = ({total, cartItems}) => (
     <div className="test-warning">
       *Please use the following test credit card for payments* 
       <br/>
-      4242 4242 4242 4242 - Exp: 01/20
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </div>
 
   </div>
