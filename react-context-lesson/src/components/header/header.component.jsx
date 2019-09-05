92aa23f1dbc97657a5e1a1db5aa86f9c4c49f59a
jordiup <jordi.hermoso.up@gmail.com>
2019-09-05 11:18:20 +0800

    Context Api - converting createContext to useContext hook, Context Provider in App.js, Redux cart > Cart Context

--- react-context-lesson/src/components/header/header.component.jsx
+++ react-context-lesson/src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -7,13 +7,20 @@ import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+import CurrentUserContext from '../../context/current-user/current-user.context';
+import CartContext from '../../context/cart/cart.context';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = () => {
+  
+  const currentUser = useContext(CurrentUserContext);
+  const [hidden, setHidden] = useState(true);
+  const toggleHidden = () => setHidden(!hidden);
+
+  return (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -34,14 +41,19 @@ const Header = ({ currentUser, hidden }) => (
           SIGN IN
         </Link>
       )}
-      <CartIcon />
+      <CartContext.Provider value={{
+        hidden,
+        toggleHidden
+      }}>
+        <CartIcon />
+      </CartContext.Provider>
+
     </div>
     {hidden ? null : <CartDropdown />}
   </div>
-);
+)};
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
   hidden: selectCartHidden
 });
 
